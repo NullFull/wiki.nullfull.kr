c7956fab87cb0ccd46a5944b2cd490f233bdcfaa
lexifdev <sl@lxf.kr>
2020-07-13 04:16:45 +0900

    환경 변수 런타임에 전달

--- next.config.js
+++ next.config.js
@@ -11,5 +11,11 @@ module.exports = {
             'utils': path.join(__dirname, 'src', 'utils'),
         }
         return config
+    },
+    env: {
+        FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+        FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+        FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+        API_ENDPOINT: process.env.API_ENDPOINT
     }
 }
