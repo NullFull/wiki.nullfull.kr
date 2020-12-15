a0bebea66164aece971e3094ca1664eaab76738a
lexifdev <sl@lxf.kr>
2020-12-16 07:19:59 +0900

    API 호출시 서버 성공 아니면 예외 던지도록

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,6 +7,11 @@ const BASE_URL = process.env.API_ENDPOINT || 'http://localhost:3000/api'
 const client = {
     get: async path => {
         const response = await fetch(BASE_URL + path)
+
+        if (!response.ok) {
+            throw new Error(`${response.status}`)
+        }
+
         const payload = await response.json()
         return [{}, payload.data]
     },
@@ -18,6 +23,11 @@ const client = {
             },
             body: JSON.stringify(data)
         })
+
+        if (!response.ok) {
+            throw new Error(`${response.status}`)
+        }
+
         const payload = await response.json()
         return [{}, payload.data]
     },
